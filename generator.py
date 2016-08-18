999c94f3e7278205bbd009b187726416855182a2
Alias Lyend <Lyend.chatango@gmail.com>
2016-08-18 02:18:18 -0500

    added tests and fixed some bugs

--- generator.py
+++ generator.py
@@ -1,7 +1,8 @@
+from time import strftime
+
 class _calendar:
-    def __init__(self, AgeInMonths):
+    def __init__(self):
       self.Mon = {
-      # Num Month NumOfDays
         1:  ('January', 31),
         2:  ('February', 28),
         3:  ('March', 31),
@@ -15,42 +16,87 @@ class _calendar:
         11: ('November', 31),
         12: ('December', 31),
         }
-      #Current data
+
       self.month = int(strftime('%m'))
-      self.day = int(strftime('%d'))
-      #How many months
-      self.dead = AgeInMonths
 
-    def generate(self):
-        # Check day
-        def __checkday():
-            # Choose month
-            _id = self.Mon[self.month]
-            # Always safer than sorry
-            if self.day > _id[1]-1:
-                data = self.day - _id[1]
-                if data == 0:
-                    # Add one extra since we took one for calucation
-                    data = 2
-            
-            else: data = self.day
-            return data
+    def generate(self, AgeInMonths, day=None):
+          if day == None:
+              self.day = int(strftime('%d'))
+          else:
+              self.day = int(day)
 
-        def __checkMonth():
-            # Math time
-            return (self.month+self.dead)
+          self.dead = AgeInMonths
+          # Check day
+          def __checkday(monthId):
+              # Choose month
+              _id = self.Mon[monthId]
+              # Always safer than sorry
+              if self.day > _id[1] - 1:
+                  data = self.day - _id[1]
+                  if data == 0:
+                      # Add one extra since we took one for calucation
+                      data = 2
 
-        def __checkYear():
-            # Our year
-            year = int(strftime("%Y"))
-            while self.dead > 12 or self.dead == 12:
-                year += +1
-                self.dead -= 12
-            return str(year)
+              else:
+                  data = self.day
+              if validate_Month_and_legal_Date(monthId, data)[2] == True:
+                return data
+              else: print('Fatal Error')
 
-        year = __checkYear()
-        month = str(__checkMonth())
-        return month+'/'+str(__checkday())+'/'+year
+          def __checkMonth():
+              # Math time
+              return (self.month + self.dead)
 
-# Example...
-# print(_calander(monthsToExpire).generate())
+          def __checkYear(mon):
+              # Our year
+              year = int(strftime("%Y"))
+              while mon >= 12 or mon == 12:
+                  year += +1
+                  mon -= 12
+              if mon == 0:
+                  mon = 1
+              return str(year), str(mon)
+
+
+          year = __checkYear(__checkMonth())
+          return year[1] + '/' + str(__checkday(int(year[1]))) + '/' + year[0]
+
+def validate_Month_and_legal_Date(mon, day):
+    oneSec = _calendar()
+    if not mon > len(oneSec.Mon) and not 0 >= mon:
+        for x in oneSec.Mon:
+            if x == mon:
+                data = oneSec.Mon[x][1]
+                if not int(day) > data and not data <= 0:
+                    trueorFalse = True
+                else:
+                    trueorFalse = False
+    else:
+        print('Bad month Error')
+        trueorFalse = False
+
+    return mon, day, trueorFalse
+
+def checkSpan(age):
+    FalseData = []
+    cal = _calendar()
+    amountOfChecks = 0
+    for month in range(1, 13):
+         for day in range(1, cal.Mon[month][1]+1):
+            amountOfChecks += 1
+            data = cal.generate(int(age), day=day).split('/')
+            newdata = validate_Month_and_legal_Date(int(data[0]), int(data[1]))
+            if newdata[2] == False:
+                FalseData.append(newdata)
+
+    if not len(FalseData) == 0:
+        for x in FalseData:
+            print(x)
+
+    print('days: '+str(amountOfChecks))
+
+def ageTest(yearCheck):
+    for x in range(1, 12*int(yearCheck)):
+        checkSpan(x)
+
+ageTest(12)
